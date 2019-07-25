d861adc2b29995e8656d16303d6f37482070cbd1
micahhowe <micahhowe182@gmail.com>
2019-07-25 15:53:31 -0600

    added multiple endpoints

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const app = express()
 const ctrl = require('./server/products_controller')
 const {SERVER_PORT, CONNECTION_STRING} = process.env
 
-app.use(express.json())
 
 massive(CONNECTION_STRING).then(dbInstance => {
     app.set('db', dbInstance);
 }).catch(err => console.log(err))
+
+app.use(express.json())
+
+app.get('/api/products', products_controller.getAll)
+app.get('/api/products/:id', products_controller.getOne)
+app.post('/api/products', products_controller.create)
+app.put('/api/products/:id', products_controller.update)
+app.delete('/api/products/:id', products_controller.delete)
 
 
 app.listen(SERVER_PORT, () => 
